b3faccbf8063fb32eb5b337dbde0df5b73fa244f
mmoritz8 <mmoritz0808@gmail.com>
2020-09-22 00:05:05 -0500

    remove from local storage

--- appes6.js
+++ appes6.js
@@ -83,10 +83,19 @@ class Store {
 
         localStorage.setItem('books', JSON.stringify(books));
     }
-    static removeBook() {
+    static removeBook(isbn) {
+        const books = Store.getBooks();
 
+        books.forEach(function (book, index) {
+            if (book.isbn === isbn) {
+                books.splice(index, 1);
+            }
+        });
+
+        localStorage.setItem('books', JSON.stringify(books));
     }
 }
+
 //Event Listers
 // DOM Load Event
 
@@ -129,6 +138,8 @@ document.getElementById('book-form').addEventListener('submit',
 document.getElementById('book-list').addEventListener('click', function (e) {
     const ui = new UI();
     ui.deleteBook(e.target);
+
+    Store.removeBook(e.target.parentElement.previousElementSibling.textContent)
 
     //show msg
 
